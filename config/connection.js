e171071640e1b8a72f2c5faf9ef51a577f9e6276
David Davies <David_Davies@mail.com>
2022-03-27 17:47:57 +0100

    added routes thought, user altered connection and server.js

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/social-network-api", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-api",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // Export connection
 module.exports = mongoose.connection;
